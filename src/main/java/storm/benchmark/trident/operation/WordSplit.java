6cc20d49d3590a0dfee7fd732e49010f105c16f4
manuzhang <owenzhang1990@gmail.com>
2014-05-29 14:41:54 +0800

    add debug log

--- src/main/java/storm/benchmark/trident/operation/WordSplit.java
+++ src/main/java/storm/benchmark/trident/operation/WordSplit.java
@@ -1,12 +1,14 @@
 package storm.benchmark.trident.operation;
 
 import backtype.storm.tuple.Values;
+import org.apache.log4j.Logger;
 import storm.trident.operation.BaseFunction;
 import storm.trident.operation.TridentCollector;
 import storm.trident.tuple.TridentTuple;
 
 public class WordSplit extends BaseFunction {
 
+  private static final Logger LOG = Logger.getLogger(WordSplit.class);
   private static final long serialVersionUID = -8605358179216330897L;
 
   public static String[] splitSentence(String sentence) {
@@ -19,6 +21,7 @@ public class WordSplit extends BaseFunction {
   @Override
   public void execute(TridentTuple tuple, TridentCollector collector) {
     String sentence = tuple.getString(0);
+    LOG.debug("receive sentence: '" + sentence + "'");
     if (sentence != null) {
       for (String word : splitSentence(sentence)) {
         collector.emit(new Values(word));
