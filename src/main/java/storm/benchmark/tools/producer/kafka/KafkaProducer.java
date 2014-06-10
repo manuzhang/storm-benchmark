73d944c649eac0cd276933c9c882ada9dfcfe6f6
manuzhang <owenzhang1990@gmail.com>
2014-06-10 15:09:59 +0800

    remove ghost StormBenchmark from last pull

--- src/main/java/storm/benchmark/tools/producer/kafka/KafkaProducer.java
+++ src/main/java/storm/benchmark/tools/producer/kafka/KafkaProducer.java
@@ -20,9 +20,15 @@ package storm.benchmark.tools.producer.kafka;
 
 import backtype.storm.Config;
 import backtype.storm.generated.StormTopology;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.IRichSpout;
+import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
 import storm.benchmark.api.IProducer;
 import storm.benchmark.util.BenchmarkUtils;
@@ -31,6 +37,7 @@ import storm.kafka.bolt.KafkaBolt;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * KafkaProducer is itself a Storm benchmarks which consists of a KafkaProducerSpout and a KafkaBolt
@@ -82,4 +89,35 @@ public abstract class KafkaProducer  implements IProducer {
     return kafkaConfig;
   }
 
+  /**
+   * KafkaProducerSpout generates source data for downstream KafkaBolt to
+   * write into Kafka. The output fields consist of BOLT_KEY and BOLT_MESSAGE.
+   * BOLT_KEY will decide the Kafka partition to write into and BOLT_MESSAGE the
+   * actual message. Users set the number of partitions and by default messages will
+   * be written into each partition in a round-robin way.
+   */
+  public static abstract class KafkaProducerSpout extends BaseRichSpout {
+
+    private static final long serialVersionUID = -3823006007489002720L;
+    private final Random random;
+    protected SpoutOutputCollector collector;
+
+    public KafkaProducerSpout() {
+      random = new Random();
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+      declarer.declare(new Fields(KafkaBolt.BOLT_KEY, KafkaBolt.BOLT_MESSAGE));
+    }
+
+    @Override
+    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+      this.collector = collector;
+    }
+
+    protected void nextMessage(String message) {
+      collector.emit(new Values(random.nextInt() + "", message));
+    }
+  }
 }
