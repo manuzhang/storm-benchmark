57aa99ef119c64c39fdfe7100a49df167790be8f
Sean Zhong <clockfly@gmail.com>
2014-10-30 19:56:32 +0800

    Update config.sh

--- benchmark/conf/config.sh
+++ benchmark/conf/config.sh
@@ -14,20 +14,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Where is the storm binary
 BIN=/usr/lib/storm/bin/storm
+
+# Absolute path to the stom-benchmark-with-dependencies.jar
 JAR=/root/storm-benchmark-0.1.0-jar-with-dependencies.jar
+
+# Please don't modify this
 MAIN_CLASS=storm.benchmark.tools.Runner
 
+# We will pull the metrics from nimbus periodically. This defines the interval.
 METRICS_POLL_INTERVAL=60000 # 60 secs
+
+ # How long will we run for each benchmark.
 METRICS_TOTAL_TIME=300000  # 5 mins
+
+# Where we store the metrics reports. The metrics contains the performance and throughput information.
 METRICS_PATH=/root/benchmark/reports
 METRICS_CONF=metrics.time=$METRICS_TOTAL_TIME,metrics.poll=$METRICS_POLL_INTERVAL,metrics.path=$METRICS_PATH
 
+# The default workers we use for the benchmark.
 WORKERS=4
+
+# The default ack tasks we use for the benchmark.
 ACKERS=$WORKERS
+
+# The default max.spout.pending(it will override the default storm config) we use for the benchmarks.
 PENDING=200
 
 ### the kafka configuration
+ # Kafka broker list [node1:port1, node2:port2, ...]
 BROKER_LIST=intelidh-04:9092
+# Zookeeper server list [node1:port1, node2:port2, ...]
 ZOOKEEPER_SERVERS=intelidh-04:2181
+# the root path you create for Kafka in Zookeeper
 KAFKA_ROOT_PATH=/kafka/kafka-cluster-0
